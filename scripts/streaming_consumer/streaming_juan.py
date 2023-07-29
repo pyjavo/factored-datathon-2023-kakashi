9a180c2a824346e576bab87f25184fd97553d110
Javier Daza <javierdazacomas@gmail.com>
2023-07-29 14:43:51 -0500

    Create parquet file from events received

--- scripts/streaming_consumer/streaming_juan.py
+++ scripts/streaming_consumer/streaming_juan.py
@@ -1,11 +1,11 @@
 import os
-import logging
+# import logging
 from datetime import datetime
 
 from dotenv import load_dotenv
 from azure.eventhub import EventHubConsumerClient
-import google.cloud.logging
-import gcsfs
+# import google.cloud.logging
+# import gcsfs
 import pandas as pd
 
 
@@ -26,40 +26,73 @@ client = EventHubConsumerClient.from_connection_string(
     eventhub_name=eventhub_name
 )
 
+column_names = [
+    'vote',
+    'style',
+    'offset',
+    'timestamp',
+    'asin',
+    'overall',
+    'reviewText',
+    'reviewerID',
+    'reviewerName',
+    'summary',
+    'verified',
+    'internal_partition',
+    'partition_number',
+    'unixReviewTime',
+    'image'
+]
+# Local path for testing
+file_path = 'reviews_' + f"{current_date}.parquet"
+# file_path = team_data_lake + f"{current_date}.parquet"
+
+
 # Configure logging
 
-logging_client = google.cloud.logging.Client()
-logging_client.setup_logging()
+# logging_client = google.cloud.logging.Client()
+# logging_client.setup_logging()
 
-
-logger = logging.getLogger("azure.eventhub")
-logging.basicConfig(level=logging.INFO)
+# === This logger prints too much information ===
+# logger = logging.getLogger("azure.eventhub")
+# logging.basicConfig(level=logging.INFO)
 
 def on_event(partition_context, event):
-    logger.info(
-        f"Received event from partition {partition_context.partition_id}"
-    )
+    # logger.info(
+    #     f"Received event from partition {partition_context.partition_id}"
+    # )
+    # logger.info(f"___Event\n {event}")
     print(f"Received event from partition {partition_context.partition_id}")
-    
-    logger.info(event)
-    print(event)
+    print(f"___Event\n {event}")  # event is EventData
 
+    jsonbody = event.body_as_json(encoding='UTF-8')  # jsonbody is a dict
+    # print(f"======== Received jsonbody =======\n {jsonbody}")
+    print()
+    print()
 
-    df = pd.read_json(event)
+    for col in column_names:
+        if col not in jsonbody.keys():
+            jsonbody[col] = None
 
-    file_path = team_data_lake + f"{current_date}.parquet"
+    df = pd.DataFrame(jsonbody, index=[0])
 
+    print()
+    print('creating file...')
+    print()
     try:
         df.to_parquet(file_path, engine='fastparquet', append=True)
-    except AttributeError as e:
+        # df.to_parquet(file_path, mode="append")
+    except FileNotFoundError:
         df.to_parquet(file_path, engine='fastparquet')
-        
-    # df.to_parquet(file_path, mode="append")
+
     partition_context.update_checkpoint(event)
 
+
 with client:
+    print('starting...')
     client.receive(
-        on_event=on_event
+        on_event=on_event,
+        starting_position="-1",
     )
     # receive events from specified partition:
     # client.receive(on_event=on_event, partition_id='0')
